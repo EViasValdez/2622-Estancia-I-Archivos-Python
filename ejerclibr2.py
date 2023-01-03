8735388266bbaed12fb77032c2847c69dbb9571f
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-01-02 22:03:00 -0600

    Add files via upload

--- ejerclibr2.py
+++
@@ -1,7 +0,0 @@
-x = int (input( "Introduzca un numero natural positivo: "))
-
-if x > 0:
-    Cubo = x * x * x
-    print (" El cubo de ", str(x), "es: ", Cubo)
-else:
-    print("El numero ingresado no es natural positivo")
