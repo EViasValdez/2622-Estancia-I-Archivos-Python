93fe715c03ae5c2bc019e1753d52cbd81716aa4e
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-01-26 21:51:23 -0600

    Add files via upload

--- F-String.py
+++ F-String.py
@@ -1,15 +1,15 @@
 class Estudiante:
-    def __init__(self, nombre, apellido, edad, municipio):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.municipio = municipio
+    def __init__(self, Nombre, Apellido, Edad, Municipio):
+        self.Nombre = Nombre
+        self.Apellido = Apellido
+        self.Edad = Edad
+        self.Municipio = Municipio
 
     def __str__(self):
-        return f"Hola mi nombre es {self.nombre} {self.apellido}, tengo {self.edad} años y vivo en {self.municipio}"
+        return f"Hola mi nombre es {self.Nombre} {self.Apellido}, tengo {self.Edad} años y vivo en {self.Municipio}"
 
     def __repr__(self):
-        return f"Hola mi nombre es {self.nombre} {self.apellido}, tengo {self.edad} años y vivo en {self.municipio}"
+        return f"Hola mi nombre es {self.Nombre} {self.Apellido}, tengo {self.Edad} años y vivo en {self.Municipio}"
 
 EstudianteNuevo = Estudiante ("Eduardo", "Viñas", "22", "Tecamac")
 
