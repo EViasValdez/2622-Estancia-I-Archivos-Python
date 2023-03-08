19a66085d65d735585d01a7b3e556a03575afc9e
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-03-08 10:19:03 -0600

    Add files via upload

--- ActividadFunciones.py
+++ ActividadFunciones.py
@@ -1,14 +1,14 @@
 print("Conversor de monedas")
 print("Por EViasValdez")
 
-def Conversor(MonedaActual, Valor, MonedaAConvertir):
+def Conversor(MonedaActual, Valor, MonedaaConvertir):
     if MonedaActual == 1:
         def ToDolar():
-            if MonedaAConvertir == "1":
+            if MonedaaConvertir == "1":
                 print(f"{Valor} dolares equivale a ${Valor * 0.49} pesos colombianos")
-            elif MonedaAConvertir == "2":
+            elif MonedaaConvertir == "2":
                 print(f"{Valor} dolares equivale a ¥{Valor * 6.37} yuanes")
-            elif MonedaAConvertir == "3":
+            elif MonedaaConvertir == "3":
                 print(f"{Valor} dolares equivale a £{Valor * 0.76} libras")
             else:
                 print("Moneda no reconocida")
@@ -16,11 +16,11 @@ def Conversor(MonedaActual, Valor, MonedaAConvertir):
 
     elif MonedaActual == 2:
         def ToEuro():
-            if MonedaAConvertir == "1":
+            if MonedaaConvertir == "1":
                 print(f"{Valor} dolares equivale a ${Valor * 4000} pesos colombianos")
-            elif MonedaAConvertir == "2":
+            elif MonedaaConvertir == "2":
                 print(f"{Valor} dolares equivale a ¥{Valor * 6.93} yuanes o yenes")
-            elif MonedaAConvertir == "3":
+            elif MonedaaConvertir == "3":
                 print(f"{Valor} dolares equivale a £{Valor * 0.83} libras")
             else:
                 print("Moneda no reconocida")
@@ -30,5 +30,5 @@ def Conversor(MonedaActual, Valor, MonedaAConvertir):
 
 MonedaActual = int(input("Ingrese su moneda actual; \n 1. Dolar \n 2. Euro \n"))
 Valor = float(input("Ingrese el valor a convertir: \n"))
-MonedaAConvertir = input("¿A que valor desea convertirlo? \n 1. Pesos Colombianos \n 2. Yuanes \n 3. Libras esterlinas \n")
-Conversor(MonedaActual, Valor, MonedaAConvertir)
+MonedaaConvertir = input("¿A que valor desea convertirlo? \n 1. Pesos Colombianos \n 2. Yuanes \n 3. Libras esterlinas \n")
+Conversor(MonedaActual, Valor, MonedaaConvertir)
