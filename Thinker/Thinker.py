9fb5b578fecb3fca8c25114761d106d6f41c2427
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-08-14 19:18:11 -0600

    Add files via upload

--- Thinker/Thinker.py
+++ Thinker/Thinker.py
@@ -8,7 +8,7 @@ PrimerFrame.pack()
 label = Label(PrimerFrame, text = "Esto es un texto")
 label.pack()
 
-imagen = PhotoImage(file = "PH.png")
-Label (PrimerFrame, image = imagen).place(x = 100, y = 200)
+Iimagen = PhotoImage(file = "PH.png")
+Label (PrimerFrame, image = Imagen).place(x = 100, y = 200)
 
 VentanaPrincipal.mainloop()
