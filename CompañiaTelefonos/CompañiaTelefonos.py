17823bb7d33486fa2c45fce3afeeede4d38e546e
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-09-09 09:16:15 -0600

    Add files via upload

--- CompañiaTelefonos/CompañiaTelefonos.py
+++ CompañiaTelefonos/CompañiaTelefonos.py
@@ -3,7 +3,7 @@ print("++++ CUATRO PESOS POR MINUTO ++++")
 input()
 
 Minutos = int(input("Ingrese los minutos de la llamada: "))
-Precio = int(input("Ingrese el precio por minutos de la llamada: "))
+Precio = int(input("Ingrese el precio por minutos de llamada: "))
 
 Total = Minutos * Precio
 print("El costo de su llamada por " + str(Minutos) + " minutos es de un total de $" + str(Total) + " pesos")
